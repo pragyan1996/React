62b4af3e6105ef3e4ef9b78c363610655b20c1cc
Rishi Tripathi <rishitripathi@Rishis-MacBook-Air.local>
2024-01-22 07:01:46 +0530

    Added the profile card as html css practice

--- try.js
+++ try.js
@@ -1,17 +1,19 @@
-function solve(a, i, dp) {
-	if (i < 0) return 0;
+function solve(a, i, n, dp) {
+	if (i < n) return 0;
 	if (dp[i] == -1) {
-		dp[i] = Math.max(solve(a, i - 1, dp), a[i] + solve(a, i - 2, dp));
+		dp[i] = Math.max(solve(a, i - 1,n, dp), a[i] + solve(a, i - 2, n, dp));
 	}
 	return dp[i];
 }
 var rob = function (nums) {
-	let dp = [];
+	let dp = [], dp2 = [], n = nums.length;
 	for (let i in nums) {
 		dp.push(-1);
+		dp2.push(-1);
 	}
-	console.log(dp);
-	return solve(nums, nums.length - 1, dp);
+	let t1 = solve(nums, n - 1, 1, dp);
+	console.log(t1);
+	let t2 = solve(nums, n - 2, 0, dp2);
+	return Math.max(t1,t2);
 };
-
-console.log(rob([1, 2, 3, 1]));
+console.log(rob([2, 3, 2]));
